b2b456761fa8f62a2f4e1c3b6e8c9bea511a0dcd
kevint1221 <32470783+kevint1221@users.noreply.github.com>
2019-03-14 05:10:36 -0800

    two palyer wqorks

--- Final_code.c
+++ Final_code.c
@@ -20,6 +20,7 @@ unsigned char tempA = 0x00; //PINA
 unsigned char tempD = 0x00; // PINB
 //output variable
 unsigned char tempLED = 0x00; //send to led
+unsigned char tempLED2 = 0xAA;
 //sensor
 int record = 0; //check if it is recording
 //button
@@ -35,15 +36,19 @@ unsigned long change_time = 10;
 unsigned char game_start = 0;
 unsigned char correct = 0;
 unsigned char wrong = 0;
+
 //songs
 int play_counter = 0; //counting for playing note
 int play_level = 0;// count how many note in the melody
 unsigned char play = 0;
 unsigned char level1[3] = { 0x01, 0x04,0x02 };// 3 note									1 3 2 
 unsigned char level2[5] = { 0x08, 0x01, 0x08, 0x10, 0x02 }; // 5 note						4 1 4 5 2
-unsigned char level3[7] = { 0x02, 0x10, 0x08, 0x10, 0x01, 0x04, 0x02 };// 7 note			2 5 4 5 1 3 2 
-unsigned char level4[5] = { 0x08, 0x04, 0x01, 0x02, 0x01 };// 5 note, faster				4 3 1 2 1 
-unsigned char level5[7] = { 0x01, 0x10, 0x04, 0x02, 0x08, 0x02, 0x01 };// 7 note faster	1 5 3 2 4 2 1 
+unsigned char level3[7] = { 0x02, 0x10, 0x08, 0x10, 0x01, 0x04, 0x02 };// 7 note			2 5 4 5 1 3 2  
+unsigned char level4[5] = { 0x01, 0x10, 0x02, 0x04, 0x01 };// 5 note, faster				1 5 2 3 1 
+unsigned char level5[8] = { 0x10, 0x08, 0x04, 0x02, 0x04, 0x08, 0x02, 0x01 };// 8 note faster 5 4 3 2 3 4 2 1
+unsigned char level6[6] = { 0x10, 0x04, 0x01, 0x04, 0x01 , 0x04, 0x01, 0x04, 0x01, 0x04, 0x10 };
+
+
 //speaker
 double wrong_sound[4] = { 146.832, 0.00, 146.832, 0.00 };
 double correct_sound[4] = { 1174.659,0.00, 987.767, 0.00 };
@@ -57,7 +62,25 @@ unsigned char *begin_words;
 unsigned char i;
 unsigned char len;
 unsigned char *leveling; //size need to be n+1
-unsigned long lcd_period = 10; //use to change tick time
+unsigned char lcd_period = 9; //use to change tick time
+unsigned char lcd_count = 0;
+unsigned char lcd_flash = 0; //flashing counter
+unsigned long shift = 0; //shift led in register
+unsigned char shift_speed = 10;
+//multiplayer
+unsigned char mult = 0;
+unsigned char thewinner = 0;
+
+struct players
+{
+	unsigned char score;
+	unsigned char on;
+
+
+};
+
+struct players p1;
+struct players p2;
 
 
 void reset(); //prototype
@@ -159,7 +182,7 @@ void game_led()
 		}
 		else if (level == 4)
 		{
-			change_time = 8; //change speed
+			change_time = 5; //change speed
 			tempLED = level4[play_counter];
 			play_counter++;
 		}
@@ -205,6 +228,14 @@ void game_level()
 			//level_state = levelone;
 			//play_level = 3;
 			start_game = 0;
+			if (mult == 1)
+			{
+				p1.score = 0;
+				p2.score = 0;
+				p1.on = 1; //p1 go first	
+				p2.on = 0;
+			}
+
 
 		}
 		break;
@@ -214,6 +245,8 @@ void game_level()
 			play = 1;
 			level_state = leveltwo;
 			play_level = 5;
+			p1.on = 0; //p2 go first
+			p2.on = 1;
 		}
 		break;
 	case leveltwo:
@@ -222,6 +255,8 @@ void game_level()
 			play = 1;
 			level_state = levelthree;
 			play_level = 7;
+			p1.on = 1; //p1 go first
+			p2.on = 0;
 		}
 		break;
 	case levelthree:
@@ -230,6 +265,8 @@ void game_level()
 			play = 1;
 			level_state = levelfour;
 			play_level = 5;
+			p1.on = 0; //p2 go first
+			p2.on = 1;
 		}
 		break;
 	case levelfour:
@@ -237,10 +274,27 @@ void game_level()
 		{
 			play = 1;
 			level_state = levelfive;
-			play_level = 7;
+			play_level = 8;
+			p1.on = 1; //p1 go first
+			p2.on = 0;
 		}
 		break;
 	case levelfive:
+		if (mult == 1)
+		{
+			if (p1.score > p2.score)
+			{
+				thewinner = 1; //p1 win
+			}
+			else if (p1.score < p2.score)
+			{
+				thewinner = 2; //p2 win
+			}
+			else
+			{
+				thewinner = 3; //fair
+			}
+		}
 		break;
 	default:
 		break;
@@ -262,6 +316,14 @@ void game_level()
 				counter += 1;
 				if (counter == 3)
 				{
+					if (p1.on == 1) //if p1 is correct
+					{
+						p1.score += 1;
+					}
+					else
+					{
+						p2.score += 1; //if p2 is correct
+					}
 					level = 2;
 					counter = 0; //reset counter
 					PORTB = SetBit(PORTB, 0, 0); //set PB0 to 0
@@ -269,11 +331,27 @@ void game_level()
 
 				}
 			}
-			else if (press != 0x00)
+			else if (press != 0x00 && mult == 0)
 			{
 				lose = 1;
 				reset();
 				//	PORTB = SetBit(PORTB, 1,1 );
+			}
+			else if (press != 0x00 && mult == 1)
+			{
+				if (p1.on == 1)
+				{
+					p1.on = 0; //p1 lose first round
+					play = 1; //play the music
+					PORTB = SetBit(PORTB, 0, 0); //set PB0 to 0
+				}
+				else //p1 and p2 all lose, go to next level
+				{
+					level = 2;
+					counter = 0; //reset counter
+					correct = 1;//enable interrupt/////////////////////////////////
+					PORTB = SetBit(PORTB, 0, 0); //set PB0 to 0
+				}
 			}
 		}
 		break;
@@ -286,16 +364,43 @@ void game_level()
 				counter += 1;
 				if (counter == 5)
 				{
+					if (p1.on == 1) //if p1 is correct
+					{
+						p1.score += 1;
+						p1.on = 0;
+					}
+					else
+					{
+						p2.score += 1; //if p2 is correct
+					}
 					level = 3;
 					counter = 0;
 					PORTB = SetBit(PORTB, 0, 0); //set PB0 to 0
 					correct = 1;
 				}
 			}
-			else if (press != 0x00)
+			else if (press != 0x00 && mult == 0)
 			{
 				lose = 1;
 				reset();
+				//	PORTB = SetBit(PORTB, 1,1 );
+			}
+			else if (press != 0x00 && mult == 1)
+			{
+				if (p2.on == 1)
+				{
+					p2.on = 0; //p2 lose second round
+					play = 1; //play the music
+					PORTB = SetBit(PORTB, 0, 0); //set PB0 to 0
+				}
+				else //p1 and p2 all lose, go to next level
+				{
+					level = 3;
+					counter = 0; //reset counter
+					correct = 1;//enable interrupt/////////////////////////////////
+					PORTB = SetBit(PORTB, 0, 0); //set PB0 to 0
+				}
+
 			}
 		}
 		break;
@@ -308,16 +413,41 @@ void game_level()
 				counter += 1;
 				if (counter == 7)
 				{
+					if (p1.on == 1) //if p1 is correct
+					{
+						p1.score += 1;
+					}
+					else
+					{
+						p2.score += 1; //if p2 is correct
+					}
 					level = 4;
 					counter = 0;
 					PORTB = SetBit(PORTB, 0, 0); //set PB0 to 0
 					correct = 1;
 				}
 			}
-			else if (press != 0x00)
+			else if (press != 0x00 && mult == 0)
 			{
 				lose = 1;
 				reset();
+				//	PORTB = SetBit(PORTB, 1,1 );
+			}
+			else if (press != 0x00 && mult == 1)
+			{
+				if (p1.on == 1)
+				{
+					p1.on = 0; //p1 lose first round
+					play = 1; //play the music
+					PORTB = SetBit(PORTB, 0, 0); //set PB0 to 0
+				}
+				else //p1 and p2 all lose, go to next level
+				{
+					level = 4;
+					counter = 0; //reset counter
+					correct = 1;//enable interrupt/////////////////////////////////
+					PORTB = SetBit(PORTB, 0, 0); //set PB0 to 0
+				}
 			}
 		}
 		break;
@@ -330,16 +460,43 @@ void game_level()
 				counter += 1;
 				if (counter == 5)
 				{
+					if (p1.on == 1) //if p1 is correct
+					{
+						p1.score += 1;
+						p1.on = 0;
+					}
+					else
+					{
+						p2.score += 1; //if p2 is correct
+					}
 					level = 5;
 					counter = 0; //reset counter
 					PORTB = SetBit(PORTB, 0, 0); //set PB0 to 0
 					correct = 1;
 				}
 			}
-			else if (press != 0x00)
+			else if (press != 0x00 && mult == 0)
 			{
 				lose = 1;
 				reset();
+				//	PORTB = SetBit(PORTB, 1,1 );
+			}
+			else if (press != 0x00 && mult == 1)
+			{
+				if (p2.on == 1)
+				{
+					p2.on = 0; //p2 lose second round
+					play = 1; //play the music
+					PORTB = SetBit(PORTB, 0, 0); //set PB0 to 0
+				}
+				else //p1 and p2 all lose, go to next level
+				{
+					level = 5;
+					counter = 0; //reset counter
+					correct = 1;//enable interrupt/////////////////////////////////
+					PORTB = SetBit(PORTB, 0, 0); //set PB0 to 0
+				}
+
 			}
 		}
 		break;
@@ -350,18 +507,42 @@ void game_level()
 			if (level5[counter] == press)
 			{
 				counter += 1;
-				if (counter == 7)
+				if (counter == 8)
 				{
+					if (p1.on == 1) //if p1 is correct
+					{
+						p1.score += 1;
+					}
+					else
+					{
+						p2.score += 1; //if p2 is correct
+					}
 					win = 1;
 					PORTB = SetBit(PORTB, 0, 0); //set PB0 to 0
 
 					//win_sound();
 				}
 			}
-			else if (press != 0x00)
+			else if (press != 0x00 && mult == 0)
 			{
 				lose = 1;
 				reset();
+				//	PORTB = SetBit(PORTB, 1,1 );
+			}
+			else if (press != 0x00 && mult == 1)
+			{
+				if (p1.on == 1)
+				{
+					p1.on = 0; //p1 lose first round
+					play = 1; //play the music
+					PORTB = SetBit(PORTB, 0, 0); //set PB0 to 0
+				}
+				else //p1 and p2 all lose, go to next level
+				{
+					counter = 0; //reset counter
+
+
+				}
 			}
 		}
 		break;
@@ -408,6 +589,7 @@ void register_setup()
 
 	//Shift in some data
 	//	SPDR = 0b01010101;        //This should light alternating LEDs
+
 	SPDR = 0xFF;
 	//Wait for SPI process to finish
 	while (!(SPSR & (1 << SPIF)));
@@ -427,7 +609,17 @@ void register_setup()
 void set_register()
 {
 	//Shift in some data
-	SPDR = 0xFF;        //This should light alternating LEDs
+	if (shift >= shift_speed)
+	{
+		if (tempLED2 == 0)
+		{
+			tempLED2 = 0xAA;
+		}
+		tempLED2 = tempLED2 / 2;
+		shift = 0;
+	}
+	shift++;
+	SPDR = tempLED2;        //This should light alternating LEDs
 	//SPDR = 0xAA;
 	//Wait for SPI process to finish
 	while (!(SPSR & (1 << SPIF)));
@@ -484,14 +676,32 @@ void set_led()
 			led_state = hold;
 
 		}
-		else if (tempA == 0x40)
+		else if (tempA == 0x40 && GetBit(PORTB, 0) == 0)
 		{
-			lcd_period = 1; //change lcd_period when enter game
+			lcd_period = 5; //change lcd_period when enter game
 			level = 1;
 			game_led_state = game_init;
 			level_state = level_init;
 			start_game = 1;
 			play = 1;
+			win = 0;
+			lose = 0;
+			PORTB = SetBit(PORTB, 2, 0);
+			PORTB = SetBit(PORTB, 1, 0);
+		}
+		else if (tempA == 0x40 && GetBit(PORTB, 0) == 1)
+		{
+			lcd_period = 5; //change lcd_period when enter game
+			level = 1;
+			game_led_state = game_init;
+			level_state = level_init;
+			start_game = 1;
+			play = 1;
+			win = 0;
+			lose = 0;
+			mult = 1;
+			PORTB = SetBit(PORTB, 1, 1);
+			PORTB = SetBit(PORTB, 0, 0);
 		}
 
 		break;
@@ -567,8 +777,7 @@ void reset()
 		play_counter = 0; //counting for playing note
 		play_level = 0;// count how many note in the melody
 		level = 1;
-		win = 0;
-		lose = 0;
+
 		play = 0;
 		led_state = led_init;
 		sensor_state = sensor_init;
@@ -644,12 +853,19 @@ void win_sound()
 				speaker_count++;
 			}
 
-			PORTB = SetBit(PORTB, 1, 1);
+			//PORTB = SetBit(PORTB, 1, 1);
 
 		}
 
 
 	}
+
+}
+
+
+
+void interrupt_fcn()
+{
 
 	if (correct == 1)
 	{
@@ -670,9 +886,6 @@ void win_sound()
 }
 
 
-
-
-
 enum lcd_states { lcd_init, lcd_begin, lcd_on, lcd_single, lcd_two, lcd_lose, lcd_win } lcd_state;
 void lcd_game()
 {
@@ -684,20 +897,65 @@ void lcd_game()
 	case lcd_begin:
 		if (tempA == 0x40)
 		{
-			PORTB = SetBit(PORTB, 1, 1);
+			//PORTB = SetBit(PORTB, 1,1);
 			lcd_state = lcd_on;
 			LCD_ClearScreen();
 		}
 		break;
 	case lcd_on:
+		if (lose == 1)
+		{
+			lcd_state = lcd_lose;
+			LCD_ClearScreen();
+
+		}
+		else if (win == 1 && mult == 0)
+		{
+			lcd_state = lcd_win;
+			LCD_ClearScreen();
+		}
+		else if (win == 1 && mult == 1)
+		{
+			lcd_state = lcd_two;
+		}
 		break;
 	case lcd_single:
 		break;
 	case lcd_two:
+		if (tempA == 0x40)
+		{
+			//PORTB = SetBit(PORTB, 1,1);
+			lcd_state = lcd_on;
+			LCD_ClearScreen();
+			lcd_flash = 0;
+		}
 		break;
 	case lcd_lose:
+		if (tempA == 0x40)
+		{
+			//PORTB = SetBit(PORTB, 1,1);
+			lcd_state = lcd_on;
+			LCD_ClearScreen();
+			lcd_flash = 0;
+		}
+		if (lcd_count == 50)
+		{
+			lcd_state = lcd_begin;
+			lcd_count = 0;
+		}
+		else
+		{
+			lcd_count++;
+		}
 		break;
 	case lcd_win:
+		if (tempA == 0x40)
+		{
+			//PORTB = SetBit(PORTB, 1,1);
+			lcd_state = lcd_on;
+			LCD_ClearScreen();
+			lcd_flash = 0;
+		}
 		break;
 	default:
 		break;
@@ -708,21 +966,108 @@ void lcd_game()
 	case lcd_init:
 		break;
 	case lcd_begin:
+		shift_speed = 15;
+		lcd_period = 9; //use to change tick time
+		LCD_ClearScreen();
 		LCD_DisplayString(1, begin_words);
 		if (i <= len) { begin_words++; i++; }
 		else { begin_words -= len; i = 1; }
 		break;
 	case lcd_on:
+		shift_speed = 15;
 		LCD_DisplayString(1, leveling);
-
 		break;
 	case lcd_single:
 		break;
 	case lcd_two:
+		if (thewinner == 1)
+		{
+			if (lcd_flash >= 3)
+			{
+				LCD_ClearScreen();
+				if (lcd_flash >= 6)
+				{
+					lcd_flash = 0;
+				}
+				lcd_flash++;
+			}
+			else
+			{
+				LCD_DisplayString(1, "    player1 is    the winner");
+				lcd_flash++;
+			}
+		}
+		else if (thewinner == 2)
+		{
+			if (lcd_flash >= 3)
+			{
+				LCD_ClearScreen();
+				if (lcd_flash >= 6)
+				{
+					lcd_flash = 0;
+				}
+				lcd_flash++;
+			}
+			else
+			{
+				LCD_DisplayString(1, "    player2 is    the winner!!!");
+				lcd_flash++;
+			}
+		}
+		else if (thewinner == 3)
+		{
+			if (lcd_flash >= 3)
+			{
+				LCD_ClearScreen();
+				if (lcd_flash >= 6)
+				{
+					lcd_flash = 0;
+				}
+				lcd_flash++;
+			}
+			else
+			{
+				LCD_DisplayString(1, "    Chill~ you guys are even!!!");
+				lcd_flash++;
+			}
+		}
 		break;
 	case lcd_lose:
+		shift_speed = 15;
+		if (lcd_flash >= 3)
+		{
+			LCD_ClearScreen();
+			LCD_DisplayString(1, " | | | | | | | || | | | | | | | ");
+			if (lcd_flash >= 6)
+			{
+				lcd_flash = 0;
+			}
+			lcd_flash++;
+		}
+		else
+		{
+			LCD_DisplayString(1, "|  YOU   ARE  |  |  LOSER  !!! |");
+			lcd_flash++;
+		}
+
 		break;
 	case lcd_win:
+		shift_speed = 1;
+		if (lcd_flash >= 2)
+		{
+			LCD_ClearScreen();
+			if (lcd_flash >= 4)
+			{
+				lcd_flash = 0;
+			}
+			lcd_flash++;
+		}
+		else
+		{
+			LCD_DisplayString(1, " Winner Winner   Chicken Dinner");
+			lcd_flash++;
+		}
+
 		break;
 	default:
 		break;
@@ -732,26 +1077,89 @@ void lcd_game()
 //convert variable into lcd text
 void text()
 {
-	if (level == 1)
+	if (mult == 0)
 	{
-		leveling = "level 1";
+		if (level == 1)
+		{
+			leveling = "level 1";
+		}
+		else if (level == 2)
+		{
+			leveling = "level 2";
+		}
+		else if (level == 3)
+		{
+			leveling = "level 3";
+		}
+		else if (level == 4)
+		{
+			leveling = "level 4";
+		}
+		else if (level == 5)
+		{
+			leveling = "level 5";
+		}
 	}
-	else if (level == 2)
+	else
 	{
-		leveling = "level 2";
+		if (level == 1)
+		{
+			if (p1.on == 1)
+			{
+				leveling = "level 1: player1";
+			}
+			else
+			{
+				leveling = "level 1: player2";
+			}
+		}
+		else if (level == 2)
+		{
+			if (p2.on == 1)
+			{
+				leveling = "level 2: player2";
+			}
+			else
+			{
+				leveling = "level 2: player1";
+			}
+		}
+		else if (level == 3)
+		{
+			if (p1.on == 1)
+			{
+				leveling = "level 3: player1";
+			}
+			else
+			{
+				leveling = "level 3: player2";
+			}
+		}
+		else if (level == 4)
+		{
+			if (p2.on == 1)
+			{
+				leveling = "level 4: player2";
+			}
+			else
+			{
+				leveling = "level 4: player1";
+			}
+		}
+		else if (level == 5)
+		{
+			if (p1.on == 1)
+			{
+				leveling = "level 4: player1";
+			}
+			else
+			{
+				leveling = "level 4: player2";
+			}
+		}
+
 	}
-	else if (level == 3)
-	{
-		leveling = "level 3";
-	}
-	else if (level == 4)
-	{
-		leveling = "level 4";
-	}
-	else if (level == 5)
-	{
-		leveling = "level 5";
-	}
+
 }
 
 int main(void)
@@ -768,12 +1176,12 @@ int main(void)
 	PWM_on();
 	TimerSet(50);
 	TimerOn();
-
 	register_setup();
 
 	//unsigned long period = 30000;
 	unsigned char win_elapsed = 0;
 	unsigned char lcd_elapsed = 0;
+	unsigned char interrupt_elapsed = 0;
 	led_state = led_init;
 	sensor_state = sensor_init;
 	game_led_state = game_init;
@@ -782,7 +1190,6 @@ int main(void)
 	game_level();	 //initialize the game first
 	lose = 0;
 	win = 0;
-
 
 	//lcd variable initiation////////////////////////
 	begin_words = "         press reset to start the game";
@@ -795,16 +1202,6 @@ int main(void)
 	while (1)
 	{
 		text();
-
-		if (lcd_elapsed >= lcd_period)
-		{
-			lcd_game();
-			lcd_elapsed = 0;
-		}
-
-
-
-		//PORTB |= RED_LED; // MAKE red RECORD SHINE
 		//Loop forever
 		tempA = ~PINA;
 		tempD = ~PIND & 0x1F;
@@ -813,6 +1210,16 @@ int main(void)
 		set_register();
 		set_sensor();
 		game_level();
+		if (interrupt_elapsed >= 2)
+		{
+			interrupt_fcn();
+			interrupt_elapsed = 0;
+		}
+		if (lcd_elapsed >= lcd_period)
+		{
+			lcd_game();
+			lcd_elapsed = 0;
+		}
 		if (win_elapsed >= 4)
 		{
 			win_sound();
@@ -828,6 +1235,6 @@ int main(void)
 		game_led_elapsed += 1;
 		win_elapsed += 1;
 		lcd_elapsed += 1;
-		//set_record();
+		interrupt_elapsed += 1;
 	}
 }
